99b22dc2139d7c83a1f46bfdee36865d8c068554
R8 <60283818+penobit@users.noreply.github.com>
2024-04-08 19:19:07 +0330

    update path definitions

--- index.php
+++ index.php
@@ -14,18 +14,19 @@ use App\Application;
 define('PENOBIT', true);
 define('HOME', dirname(__FILE__));
 define('DB_PATH', HOME.'/database');
+define('CORE_PATH', HOME.'/core');
 define('CONFIG_PATH', HOME.'/configs');
 define('CONTENT_PATH', HOME.'/content');
 define('THEMES_PATH', CONTENT_PATH.'/themes');
-define('APP_PATH', HOME.'/core/app');
-define('STORAGE_PATH', HOME.'/core/app/storage');
-define('LOGS_PATH', HOME.'/core/app/storage/logs');
+define('APP_PATH', CORE_PATH.'/App');
+define('STORAGE_PATH', APP_PATH.'/storage');
+define('LOGS_PATH', STORAGE_PATH.'/logs');
 define('CMS_TEMPLATE_PATH', STORAGE_PATH.'/templates');
 define('CACHE_PATH', STORAGE_PATH.'/cache');
 define('TEMPLATE_CACHE_PATH', CACHE_PATH.'/templates');
 
 /** @var Application $app */
-$app = require HOME.'/core/app/bootstrap.php';
+$app = require APP_PATH.'/bootstrap.php';
 
 // Run the application
 $app->run();
